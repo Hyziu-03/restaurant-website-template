b6344258ab50323357cb7197c3ad8d2b0e5e859b
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-07-18 14:01:10 +0200

    Header's addition

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1 className='heading'>Your Restaurant Name</h1>
+            <section>
+                <button className="theme-toggler"></button>
+                <nav>
+                    <button className="navigation"></button>
+                </nav>
+            </section>
+        </header>
+    )
+}
+
+export default Header;
