16eac6691e97944f1fa0366ef49641fdddf2aa77
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-07-19 11:20:44 +0200

    Header styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Header from './components/Header'
+import Header from './components/Header';
+import LandingPage  from './components/LandingPage';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <LandingPage />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 const Header = () => {
     return (
-        <header className='header'>
-            <h1 className='heading'>Your Restaurant Name</h1>
-            <section>
-                <button className="theme-toggler"></button>
+        <header className='header' tabIndex="0">
+            <h1 className='heading' tabIndex="0">Your Restaurant Name</h1>
+            <section className="buttons">
+                <button className="theme-toggler" tabIndex="0">
+                    <i class="fas fa-sun"></i>
+                </button>
                 <nav>
-                    <button className="navigation"></button>
+                    <button className="navigation" tabIndex="0">
+                        <i class="fas fa-ellipsis-v"></i>
+                    </button>
                 </nav>
             </section>
         </header>
