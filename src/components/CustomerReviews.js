32682c0fea858cdbd316a3706078d831cefcd960
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-08-02 13:08:10 +0200

    Making customer reviews work

--- src/components/CustomerReviews.js
+++ src/components/CustomerReviews.js
@@ -1,10 +1,33 @@
 import KateSwift from '../kate-swift.jpg';
+import JohnDoe from '../john-doe.jpg';
+import MattSmith from '../matt-smith.jpg';
+
+// $ Check this: https://www.cloudhadoop.com/reactjs-display-image-url/.
+
+let currentSlide = 'Kate Swift';
+
+const changeSlides = () => {
+    if(currentSlide === 'Kate Swift') {
+        currentSlide = 'John Doe';
+
+
+    } else if(currentSlide === 'John Doe') {
+        currentSlide = 'Math Smith';
+
+
+    } else {
+        currentSlide = 'Kate Swift';
+
+
+    }
+    console.log('You are viewing ' + currentSlide);
+}
 
 const CustomerReviews = () => {
     return (
         <article className='customer-reviews' id="customer-reviews">
-            <i className="fas fa-chevron-circle-left" tabIndex="0"></i>
-            <img src={KateSwift} alt="" className="customer-photo" />
+            <i className="fas fa-chevron-circle-left" tabIndex="0" onClick={changeSlides}></i>
+            <img src={KateSwift} alt="" className="customer-photo"/>
             <section className='feedback'>
                 <p className="customer-words">
                     Vivamus non blandit lacus. Morbi laoreet bibendum ipsum, eget gravida magna cursus vitae. Phasellus
@@ -14,7 +37,7 @@ const CustomerReviews = () => {
                 </p>
                 <span className="signature">Kate Swift</span>
             </section>
-            <i className="fas fa-chevron-circle-right" tabIndex="0"></i>
+            <i className="fas fa-chevron-circle-right" tabIndex="0" onClick={changeSlides}></i>
         </article>  
     )
 }
