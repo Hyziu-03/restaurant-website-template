025ab2d531b1abd4cc510793564cc9fe797b9305
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-08-03 11:37:27 +0200

    Customer reviews' addition

---
+++ src/components/JohnDoe.js
@@ -0,0 +1,9 @@
+import JohnDoePhoto from '../john-doe.jpg';
+
+const JohnDoe = () => {
+    return (
+        <img src={JohnDoePhoto} alt="" className="customer-photo"/>
+    )
+}
+
+export default JohnDoe;
---
+++ src/components/MattSmith.js
@@ -0,0 +1,9 @@
+import MattSmithPhoto from '../matt-smith.jpg';
+
+const MattSmith = () => {
+    return (
+        <img src={MattSmithPhoto} alt="" className="customer-photo"/>
+    )
+}
+
+export default MattSmith;
