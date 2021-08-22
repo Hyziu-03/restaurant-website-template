e6e0dc701ef5928c8b4c57e5dba69f87436f8eca
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-08-22 19:02:30 +0200

    Code's shortening

--- src/components/customers/JohnDoe.js
+++ src/components/customers/JohnDoe.js
@@ -1,9 +1,3 @@
 import Photo from '../../img/john-doe.jpg';
-
-const JohnDoe = () => {
-    return (
-        <img src={Photo} alt="" className="customer-photo" />
-    )
-}
-
+const JohnDoe = () => <img src={Photo} alt="" className="customer-photo" />;
 export default JohnDoe;
--- src/components/customers/KateSwift.js
+++ src/components/customers/KateSwift.js
@@ -1,9 +1,3 @@
 import Photo from '../../img/kate-swift.jpg';
-
-const KateSwift = () => {
-    return (
-        <img src={Photo} alt="" className="customer-photo" />
-    )
-}
-
+const KateSwift = () => <img src={Photo} alt="" className="customer-photo" />;
 export default KateSwift;
