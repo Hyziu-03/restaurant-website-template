60eb0b1d90ff0f08d81cbe2f4eac64cfe232c725
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-08-24 15:40:52 +0200

    Bug fixes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ const Menu = () => {
     return (
         <section className="menu">
             <h1 className="dish-category">You are viewing Proin blandit</h1>
-            <span className="close-modalbox">&times;</span>
+            <i className="fas fa-times"></i>
             <ul className="dish-list">
                 <li className="dish">Sed fermentum vulputate <span className="price">14.00 PLN</span></li>
                 <li className="dish">Sapien nec lobortis libero <span className="price">11.00 PLN</span></li>
