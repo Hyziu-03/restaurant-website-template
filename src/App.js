cb2ba1022a181f01aeee2c7763047ea60756586f
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-09-01 14:04:40 +0200

    Security enhancement

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Header from './components/Header';
 import LandingPage  from './components/LandingPage';
 import CustomerReviews from './components/CustomerReviews';
 import Footer from './components/Footer';
-import Menu from './components/Menu';
+import Menu from './components/modal-boxes/Menu';
+
+import { BrowserRouter as Router } from "react-router-dom";
 
 const loadStylesheet = () => {
   let theme = localStorage.getItem('theme');
   let styleElement = document.getElementById('stylesheet-container');
-  theme === 'dark' ? styleElement.innerHTML = "html { background: #303030 !important; } header { background: #303030 !important; border-color: #f8f8f8 !important; } .heading { color: #f8f8f8 !important; } .heading:hover, .heading:focus-visible { text-shadow: 0 4px 4px #f8f8f8 !important; } .heading:active { color: #37e21c !important; } .theme-toggler { background: #f8f8f8 !important; color: #303030 !important; } .theme-toggler:hover { background: #37e21c !important; } .theme-toggler:active { background: #192c6c !important; } .landing-page { color: white !important; } .browse-our-menu:active, .see-customer-reviews:active { background: #192c6c !important; } .browse-our-menu { background: #f8f8f8 !important; color: #303030 !important; } .see-customer-reviews { background: #303030 !important; color: #f8f8f8 !important; -webkit-box-shadow: none !important; box-shadow: none !important; border-radius: 40px !important; border: 1px solid #f8f8f8 !important; } .customer-reviews, .footer { color: #f8f8f8 !important; background: #192c6c !important; } .contact-us { background: #303030 !important; color: #f8f8f8 !important; border-color: #f8f8f8 !important; } .contact-us:active { background: #192c6c !important; }" : styleElement.innerHTML = '';
+  theme === 'dark' ? styleElement.innerText = "html { background: #303030 !important; } header { background: #303030 !important; border-color: #f8f8f8 !important; } .heading { color: #f8f8f8 !important; } .heading:hover, .heading:focus-visible { text-shadow: 0 4px 4px #f8f8f8 !important; } .heading:active { color: #37e21c !important; } .theme-toggler { background: #f8f8f8 !important; color: #303030 !important; } .theme-toggler:hover { background: #37e21c !important; } .theme-toggler:active { background: #192c6c !important; } .landing-page { color: white !important; } .browse-our-menu:active, .see-customer-reviews:active { background: #192c6c !important; } .browse-our-menu { background: #f8f8f8 !important; color: #303030 !important; } .see-customer-reviews { background: #303030 !important; color: #f8f8f8 !important; -webkit-box-shadow: none !important; box-shadow: none !important; border-radius: 40px !important; border: 1px solid #f8f8f8 !important; } .customer-reviews, .footer { color: #f8f8f8 !important; background: #192c6c !important; } .contact-us { background: #303030 !important; color: #f8f8f8 !important; border-color: #f8f8f8 !important; } .contact-us:active { background: #192c6c !important; }" : styleElement.innerText = '';
 }
 
 const setTheme = () => {  
@@ -36,15 +38,17 @@ function App() {
 
   return (
       <div className="wrapper">
-        <div id="container" onLoad={setTheme}>
-          <Header />
-          <main>
-            <LandingPage />
-            <CustomerReviews />
-          </main>
-          <Footer />
-        </div>
-        <div id="placeholder"><Menu /></div>
+        <Router>
+          <div id="container" onLoad={setTheme}>
+            <Header />
+            <main>
+              <LandingPage />
+              <CustomerReviews />
+            </main>
+            <Footer />
+          </div>
+          <div id="placeholder"><Menu /></div>
+        </Router>
       </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 const loadStylesheet = () => {
     let theme = localStorage.getItem('theme');
     let styleElement = document.getElementById('stylesheet-container');
-    theme === 'dark' ? styleElement.innerHTML = "html { background: #303030 !important; } header { background: #303030 !important; border-color: #f8f8f8 !important; } .heading { color: #f8f8f8 !important; } .heading:hover, .heading:focus-visible { text-shadow: 0 4px 4px #f8f8f8 !important; } .heading:active { color: #37e21c !important; } .theme-toggler { background: #f8f8f8 !important; color: #303030 !important; } .theme-toggler:hover { background: #37e21c !important; } .theme-toggler:active { background: #192c6c !important; } .landing-page { color: white !important; } .browse-our-menu:active, .see-customer-reviews:active { background: #192c6c !important; } .browse-our-menu { background: #f8f8f8 !important; color: #303030 !important; } .see-customer-reviews { background: #303030 !important; color: #f8f8f8 !important; -webkit-box-shadow: none !important; box-shadow: none !important; border-radius: 40px !important; border: 1px solid #f8f8f8 !important; } .customer-reviews, .footer { color: #f8f8f8 !important; background: #192c6c !important; } .contact-us { background: #303030 !important; color: #f8f8f8 !important; border-color: #f8f8f8 !important; } .contact-us:active { background: #192c6c !important; }" : styleElement.innerHTML = '';
+    theme === 'dark' ? styleElement.innerText = "html { background: #303030 !important; } header { background: #303030 !important; border-color: #f8f8f8 !important; } .heading { color: #f8f8f8 !important; } .heading:hover, .heading:focus-visible { text-shadow: 0 4px 4px #f8f8f8 !important; } .heading:active { color: #37e21c !important; } .theme-toggler { background: #f8f8f8 !important; color: #303030 !important; } .theme-toggler:hover { background: #37e21c !important; } .theme-toggler:active { background: #192c6c !important; } .landing-page { color: white !important; } .browse-our-menu:active, .see-customer-reviews:active { background: #192c6c !important; } .browse-our-menu { background: #f8f8f8 !important; color: #303030 !important; } .see-customer-reviews { background: #303030 !important; color: #f8f8f8 !important; -webkit-box-shadow: none !important; box-shadow: none !important; border-radius: 40px !important; border: 1px solid #f8f8f8 !important; } .customer-reviews, .footer { color: #f8f8f8 !important; background: #192c6c !important; } .contact-us { background: #303030 !important; color: #f8f8f8 !important; border-color: #f8f8f8 !important; } .contact-us:active { background: #192c6c !important; }" : styleElement.innerText = '';
 }
 
 const changeTheme = () => {
@@ -16,7 +18,7 @@ const changeTheme = () => {
 const Header = () => {
     return (
         <header className='header' tabIndex="0">
-            <span className="heading"><h1>Your Name</h1></span>
+            <Link to='/home' className="heading"><h1>Your Name</h1></Link>
             <section className="buttons">
                 <button className="theme-toggler" tabIndex="0" onClick={changeTheme}>
                     <i className="fas fa-sun"></i>
