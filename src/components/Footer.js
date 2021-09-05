f300c16b81db64c6f33cbd263bcabed1f9924801
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-09-05 14:58:30 +0200

    Making the contact form work

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,31 @@
 import { Link } from "react-router-dom";
 
+const hideModal = () => {
+    document.getElementById('placeholder').style.display = 'none';
+    document.getElementById('container').style.filter = '';
+    document.getElementById('container').style.pointerEvents = 'auto';
+}
+
+const showModal = () => {
+    document.getElementById('placeholder').style.display = 'block';
+    document.getElementById('container').style.filter = 'blur(8px)';
+    document.getElementById('container').style.pointerEvents = 'none';
+}
+
+const detectClick = () => {
+    window.addEventListener('click', (e) => {
+        if (e.target === document.getElementsByClassName('fa-times')[0]) {
+            hideModal();
+        }
+    });
+}
+
 const Footer = () => {
+    detectClick();
+    
     return (
-        <footer className='footer'>
-            <Link to='/contact-us'><button className="contact-us" tabIndex="0">Contact Us</button></Link>
+        <footer className='footer' onLoad={hideModal}>
+            <Link to='/contact-us'><button className="contact-us" tabIndex="0" onClick={showModal}>Contact Us</button></Link>
             <p className="copyright">
                 This website template has been designed and developed by <strong>Szymon Hyziak</strong>.
                 Find me on LinkedIn using the link <a href="https://www.linkedin.com/in/szymon-hyziak/" className='link' target="_blank" rel="noreferrer">here</a>. Thank you!
