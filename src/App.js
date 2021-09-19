eb2776d1ffd9bc6df0fcf5b898d36eeb3f1a97e6
Szymon Hyziak <szymon.hyziak@gmail.com>
2021-09-19 13:17:18 +0200

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/Header';
 import LandingPage  from './components/LandingPage';
@@ -6,9 +6,6 @@ import CustomerReviews from './components/CustomerReviews';
 import Footer from './components/Footer';
 import Menu from './components/modal-boxes/Menu';
 import Contact from './components/modal-boxes/Contact';
-
-// ? const loadStylesheet = () => { let theme = localStorage.getItem('theme'); let styleElement = document.getElementById('stylesheet-container'); theme === 'dark' ? styleElement.innerText = "html { background: #303030 !important; } header { background: #303030 !important; border-color: #f8f8f8 !important; } .heading { color: #f8f8f8 !important; } .heading:hover, .heading:focus-visible { text-shadow: 0 4px 4px #f8f8f8 !important; } .heading:active { color: #37e21c !important; } .theme-toggler { background: #f8f8f8 !important; color: #303030 !important; } .theme-toggler:hover { background: #37e21c !important; } .theme-toggler:active { background: #192c6c !important; } .landing-page { color: white !important; } .browse-our-menu:active, .see-customer-reviews:active { background: #192c6c !important; } .browse-our-menu { background: #f8f8f8 !important; color: #303030 !important; } .see-customer-reviews { background: #303030 !important; color: #f8f8f8 !important; -webkit-box-shadow: none !important; box-shadow: none !important; border-radius: 40px !important; border: 1px solid #f8f8f8 !important; } .customer-reviews, .footer { color: #f8f8f8 !important; background: #192c6c !important; } .contact-us { background: #303030 !important; color: #f8f8f8 !important; border-color: #f8f8f8 !important; } .contact-us:active { background: #192c6c !important; }" : styleElement.innerText = ''; }
-// ? const setTheme = () => { try { if (localStorage.getItem('theme') === null) { localStorage.setItem('theme', 'light'); } loadStylesheet(); } catch (exception) { throw new Error (exception); } }
 
 const preloadImage = (url) => {
   try {
@@ -41,11 +38,11 @@ function App() {
   awaitContent();
 
   return (
-      <div className="wrapper">
+      <div className='wrapper'>
         <Router>
-          <div id="container">
-            <section id="loader" className="loader">
-                <div className="lds-default"> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div><div></div> </div>
+          <div id='container'>
+            <section id='loader' className='loader'>
+                <div className='lds-default'> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div> <div></div><div></div> </div>
             </section>
             <Header />
             <main>
@@ -54,7 +51,7 @@ function App() {
             </main>
             <Footer />
           </div>
-          <div id="placeholder"><Menu /> <Contact /></div>
+          <div id='placeholder'><Menu /> <Contact /></div>
         </Router>
       </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
-import { Link } from "react-router-dom";
-
-// ? const loadStylesheet = () => { let theme = localStorage.getItem('theme'); let styleElement = document.getElementById('stylesheet-container'); theme === 'dark' ? styleElement.innerText = "html { background: #303030 !important; } header { background: #303030 !important; border-color: #f8f8f8 !important; } .heading { color: #f8f8f8 !important; } .heading:hover, .heading:focus-visible { text-shadow: 0 4px 4px #f8f8f8 !important; } .heading:active { color: #37e21c !important; } .theme-toggler { background: #f8f8f8 !important; color: #303030 !important; } .theme-toggler:hover { background: #37e21c !important; } .theme-toggler:active { background: #192c6c !important; } .landing-page { color: white !important; } .browse-our-menu:active, .see-customer-reviews:active { background: #192c6c !important; } .browse-our-menu { background: #f8f8f8 !important; color: #303030 !important; } .see-customer-reviews { background: #303030 !important; color: #f8f8f8 !important; -webkit-box-shadow: none !important; box-shadow: none !important; border-radius: 40px !important; border: 1px solid #f8f8f8 !important; } .customer-reviews, .footer { color: #f8f8f8 !important; background: #192c6c !important; } .contact-us { background: #303030 !important; color: #f8f8f8 !important; border-color: #f8f8f8 !important; } .contact-us:active { background: #192c6c !important; }" : styleElement.innerText = ''; }
-// ? const changeTheme = () => { try { localStorage.getItem('theme') === 'light' ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light'); loadStylesheet(); } catch (exception) { throw new Error(exception); } }
+import { Link } from 'react-router-dom';
 
 const refresh = () => window.location.reload(true);
 
 const Header = () => {
     return (
         <header className='header'>
-            <Link to='/home' className="heading" onClick={refresh}><h1>Your Name</h1></Link>
-            { /* <section className="buttons"> <button className="theme-toggler" tabIndex="0" onClick={changeTheme}> <i className="fas fa-sun"></i> </button> </section> */ }
+            <Link to='/home' className='heading' onClick={refresh}><h1>Your Name</h1></Link>
         </header>
     )
 }
